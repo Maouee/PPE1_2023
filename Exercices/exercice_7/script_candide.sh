74e1296cae22b97904ff1d3032161aabd1415938
Maoui <maoui@MacBook-Pro-de-Maoui.local>
2023-11-09 22:01:56 +0100

    exercice 2 fini

--- Exercices/exercice_7/script_candide.sh
+++ Exercices/exercice_7/script_candide.sh
@@ -1,7 +1,7 @@
 #verification
-nombre_arguments_attendus=2
+nombre_arguments_attendus=1
 
-if [ $# -ne $nombre_arguments_attendus ] 
+if [ "$#" -lt 1 ] | [ "$#" -gt 2 ] 
 then
     echo "Le nombre d'arguments attendus est invalide : $#"
     echo "Ce script demande : $nombre_arguments_attendus argument(s)." 
@@ -13,7 +13,7 @@ fichier=$1
 nombre_mots=${2:-25}
 
 #traitement
-resultat=$(tr "[A-Z]" "[a-z]" < $fichier | tr -d "[:punct:]" | grep -o -E "\w+" | uniq -c | sort -r )
+resultat=$(tr "[A-Z]" "[a-z]" < $fichier | tr -d "[:punct:]" | grep -o -E "\w+" | sort | uniq -c | sort -r )
 
 #Affichage
 echo "$resultat" | head -n "$nombre_mots"
